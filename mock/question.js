5fda191f4dda7e83a668733187642c5624fb9142
wangqiaoling <1113584801@qq.com>
2023-09-18 09:13:18 +0800

    feat: 添加不同组件mock数据

--- mock/question.js
+++ mock/question.js
@@ -3,6 +3,7 @@ const Random = Mock.Random
 const getQuestionList = require('./data/getQuestionList')
 
 module.exports = [
+    // 获取单个问卷信息
     {
         url: '/api/question/:id',
         method: 'get',
@@ -11,7 +12,68 @@ module.exports = [
                 errno: 0,
                 data: {
                     id: Random.id(),
-                    title:Random.ctitle()
+                    title:Random.ctitle(),
+                    // 组件列表
+                    componentList: [
+                        {
+                            fe_id: Random.id(),
+                            type: 'questionInfo',
+                            title: '输入框2',
+                            isHidden: false,
+                            isLocked: false,
+                            props: {
+                                title: '问卷信息111',
+                                desc: '12231231'
+                            }
+                        },
+                        {
+                            fe_id: Random.id(),
+                            // 组件类型，不可重复，前后端统一
+                            type: 'questionTitle',
+                            title: '标题',
+                            isHidden: false,
+                            isLocked: false,
+                            // 组件的属性
+                            props: {
+                                text: '个人信息调研',
+                                level: 1,
+                                isCenter: false
+                            }
+                        },
+                        {
+                            fe_id: Random.id(),
+                            type: 'questionInput',
+                            title: '输入框',
+                            isHidden: false,
+                            isLocked: false,
+                            props: {
+                                title: '你的姓名',
+                                placeholder: '请输入姓名'
+                            }
+                        },
+                        {
+                            fe_id: Random.id(),
+                            type: 'questionInput',
+                            title: '输入框2',
+                            isHidden: false,
+                            isLocked: false,
+                            props: {
+                                title: '你的电话',
+                                placeholder: '请输入电话'
+                            }
+                        },
+                        {
+                            fe_id: Random.id(),
+                            type: 'questionParagraph',
+                            title: '输入框2',
+                            isHidden: false,
+                            isLocked: false,
+                            props: {
+                                text: '这是来自服务端的一行段落',
+                                isCenter: true
+                            }
+                        }
+                    ]
                 }
             }
         }
